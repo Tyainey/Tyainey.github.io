fc490173c04b69d7272f5f8d204af535d553a11f
Tyainey <tyainey.johnson@jpschools.org>
2025-02-18 21:29:28 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -18,7 +18,7 @@ $(function () {
 
     // Create walls - do not delete or modify this code
     createPlatform(-50, -50, canvas.width + 100, 50); // top wall
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "navy"); // bottom wall
+    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "pink"); // bottom wall
     createPlatform(-50, -50, 50, canvas.height + 500); // left wall
     createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
 
@@ -27,22 +27,39 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
-
+    toggleGrid();
+    createPlatform(150, 650, 150, 10, "pink");
+    createPlatform(350, 600, 200, 10, "pink"); 
+    createPlatform(600, 550, 100, 10, "pink");  
+    createPlatform(800, 500, 150, 10, "pink");
+    createPlatform(1000, 10, 300, 600, "pink");
+    
+    // TODO 3 - Create Collectables
+    createCollectable("steve", 250, 630); 
+    createCollectable("database", 450, 580);
+    createCollectable("steve", 650, 530);
+    createCollectable("database", 900, 470);
+    
+    // TODO 4 - Create Cannons
+    createCannon("bottom", 200, 1400);
+    createCannon("left", 400, 1600);
+    createCannon("bottom", 500, 900);
+    createCannon("left", 800, 1200);
+    createCannon("right", 1000, 1700);
+    createCannon("top", 900, 1700);
+    
 
     // TODO 2 - Create Platforms
-
-
-
+    
+  
 
     // TODO 3 - Create Collectables
-
+    
 
 
     
     // TODO 4 - Create Cannons
-
-
+    
     
     
     //////////////////////////////////
